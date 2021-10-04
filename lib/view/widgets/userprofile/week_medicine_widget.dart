4eeae567478d19dfb31c41be05f7b4a50ae5aa4f
DESKTOP-MP3TR33\aisayam <aisayam.sayam@gmail.com>
2021-10-04 23:24:06 +0600

    added arrows by the week med widget

--- lib/view/widgets/userprofile/week_medicine_widget.dart
+++ lib/view/widgets/userprofile/week_medicine_widget.dart
@@ -16,206 +16,228 @@ class _NextWeekMedicineState extends State<NextWeekMedicine> {
         margin: EdgeInsets.symmetric(
           vertical: 10.0,
         ),
-        padding: EdgeInsets.only(left: 50, right: 50),
         height: 100,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Card(
-                  margin: EdgeInsets.only(right: 40),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: kPrimaryColor,
-                      width: 3,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    child: Container(
-                      width: 70,
-                      height: 70,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Center(
-                            child: Text(
-                              '21',
-                              style: TextStyle(
-                                color: kPrimaryColor,
-                                fontSize: 35,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(right: 40),
-                  child: Center(
-                    child: Text(
-                      'Napa',
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 15, right: 20),
+              child: Icon(
+                Icons.keyboard_arrow_left_sharp,
+                color: kPrimaryColor,
+              ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Card(
-                  margin: EdgeInsets.only(right: 40),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: kPrimaryColor,
-                      width: 3,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    child: Container(
-                      width: 70,
-                      height: 70,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Center(
-                            child: Text(
-                              '07',
-                              style: TextStyle(
-                                color: kPrimaryColor,
-                                fontSize: 35,
-                              ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Card(
+                        margin: EdgeInsets.only(right: 35),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: kPrimaryColor,
+                            width: 3,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: InkWell(
+                          splashColor: Colors.blue.withAlpha(30),
+                          child: Container(
+                            width: 70,
+                            height: 70,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Center(
+                                  child: Text(
+                                    '21',
+                                    style: TextStyle(
+                                      color: kPrimaryColor,
+                                      fontSize: 35,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(right: 40),
-                  child: Center(
-                    child: Text(
-                      'Indomnate',
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontSize: 15,
+                      Container(
+                        margin: EdgeInsets.only(right: 35),
+                        child: Center(
+                          child: Text(
+                            'Napa',
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ),
-              ],
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Card(
+                        margin: EdgeInsets.only(right: 35),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: kPrimaryColor,
+                            width: 3,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: InkWell(
+                          splashColor: Colors.blue.withAlpha(30),
+                          child: Container(
+                            width: 70,
+                            height: 70,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Center(
+                                  child: Text(
+                                    '07',
+                                    style: TextStyle(
+                                      color: kPrimaryColor,
+                                      fontSize: 35,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 35),
+                        child: Center(
+                          child: Text(
+                            'Indomnate',
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Card(
+                        margin: EdgeInsets.only(right: 35),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: kPrimaryColor,
+                            width: 3,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: InkWell(
+                          splashColor: Colors.blue.withAlpha(30),
+                          child: Container(
+                            width: 70,
+                            height: 70,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Center(
+                                  child: Text(
+                                    '17',
+                                    style: TextStyle(
+                                      color: kPrimaryColor,
+                                      fontSize: 35,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 35),
+                        child: Center(
+                          child: Text(
+                            'Max Pro',
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Card(
+                        margin: EdgeInsets.only(right: 35),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: kPrimaryColor,
+                            width: 3,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: InkWell(
+                          splashColor: Colors.blue.withAlpha(30),
+                          child: Container(
+                            width: 70,
+                            height: 70,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Center(
+                                  child: Text(
+                                    '14',
+                                    style: TextStyle(
+                                      color: kPrimaryColor,
+                                      fontSize: 35,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(right: 35),
+                        child: Center(
+                          child: Text(
+                            'Paracetamol',
+                            style: TextStyle(
+                              color: kPrimaryColor,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Card(
-                  margin: EdgeInsets.only(right: 40),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: kPrimaryColor,
-                      width: 3,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    child: Container(
-                      width: 70,
-                      height: 70,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Center(
-                            child: Text(
-                              '17',
-                              style: TextStyle(
-                                color: kPrimaryColor,
-                                fontSize: 35,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(right: 40),
-                  child: Center(
-                    child: Text(
-                      'Max Pro',
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Card(
-                  margin: EdgeInsets.only(right: 40),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: kPrimaryColor,
-                      width: 3,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    child: Container(
-                      width: 70,
-                      height: 70,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Center(
-                            child: Text(
-                              '14',
-                              style: TextStyle(
-                                color: kPrimaryColor,
-                                fontSize: 35,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(right: 40),
-                  child: Center(
-                    child: Text(
-                      'Paracetamol',
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+            Container(
+              padding: EdgeInsets.only(left: 20, right: 15),
+              child: Icon(
+                Icons.keyboard_arrow_right_sharp,
+                color: kPrimaryColor,
+              ),
             ),
           ],
         ),
