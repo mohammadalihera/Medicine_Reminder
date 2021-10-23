a2bfaf9bfdb9e3bf83c992af6a67d4323f021f27
DESKTOP-MP3TR33\aisayam <aisayam.sayam@gmail.com>
2021-10-23 21:28:58 +0600

    updated app anme

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
-import 'package:Vitals/controller/auth_user_controller.dart';
-import 'package:Vitals/database/vital_reprository.dart';
-import 'package:Vitals/model/medicine_model.dart';
-import 'package:Vitals/view/pages/home/home_page.dart';
-import 'package:Vitals/view/pages/sign_up/sign_up.dart';
+import 'package:Vitel/view/pages/home/home_page.dart';
+import 'package:Vitel/view/pages/sign_up/sign_up.dart';
 import 'package:country_code_picker/country_localizations.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:Vitals/controller/sign_in_controller.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sqflite/sqflite.dart';
+
+import 'controller/auth_user_controller.dart';
 
 String dashname = 'User';
 
